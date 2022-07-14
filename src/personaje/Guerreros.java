bc040038dbcef84d78bf1113ec63e35a62f1975c
juansevas99 <juansevas992010@gmail.com>
2022-07-14 16:13:23 -0500

    Se creo toda la logica

---
+++ src/personaje/Guerreros.java
@@ -0,0 +1,48 @@
+package personaje;
+
+
+import TraitFactory.*;
+import funcionalidades.caminarRapido;
+import funcionalidades.golpearArma;
+import funcionalidades.hablar_informal;
+
+public class Guerreros  extends personaje{	
+	public Guerreros() {
+		super();
+		this.nombre="Guerreros";
+		
+		//rasgos de guerrro
+		TipoRasgo tipoBoca=new contruirBoca() ;
+		boca=tipoBoca.construirRasgo("pequena");
+		
+		TipoRasgo tipoOjo=new contruirOjo();
+		ojos=tipoOjo.construirRasgo("japones");
+		
+		TipoRasgo tipoNariz=new contruirNariz() ;
+		nariz=tipoNariz.construirRasgo("larga");
+		
+		TipoRasgo tipoContorno=new contruirContorno() ;
+		contorno=tipoContorno.construirRasgo("curvo");
+		
+		
+		// funcionalidades
+		
+		this.caminar=new caminarRapido();
+		this.golpear= new golpearArma();
+		this.hablar= new hablar_informal();
+		
+		
+		
+	}
+	
+
+	@Override
+	public void añadirPersonaje() {
+		this.listaPersonajes.add(new Guerreros());
+	}
+	
+	
+	
+	
+	
+}
---
+++ src/personaje/personaje.java
@@ -0,0 +1,107 @@
+package personaje;
+import TraitFactory.TipoRasgo;
+import TraitFactory.contruirBoca;
+import TraitFactory.contruirContorno;
+import TraitFactory.contruirNariz;
+import TraitFactory.contruirOjo;
+import funcionalidades.*;
+import traits.*;
+import java.util.ArrayList;
+public abstract class personaje {
+	protected String nombre;
+	protected rasgo ojos;
+	protected rasgo nariz;
+	protected rasgo boca;
+	protected rasgo contorno;
+	protected rasgo cabello;
+	protected rasgo cabeza;
+	public caminar_tipo caminar;
+	public golpear_tipo golpear;
+	public hablar_tipo hablar;
+	
+	public static ArrayList<personaje> listaPersonajes= new  ArrayList<personaje>();
+	
+	public personaje () {
+		
+		
+		
+	}
+	
+	public void cambiarOjos(String tipo) {
+		TipoRasgo rasgo= new contruirOjo();
+		ojos=rasgo.construirRasgo(tipo);
+	}
+	
+	public void cambiarBoca(String tipo) {
+		TipoRasgo rasgo= new contruirBoca();
+		boca=rasgo.construirRasgo(tipo);
+	}
+	
+	public void cambiaNariz(String tipo) {
+		TipoRasgo rasgo= new contruirNariz();
+		ojos=rasgo.construirRasgo(tipo);
+	}
+	public void cambiaContorno(String tipo) {
+		TipoRasgo rasgo= new contruirContorno();
+		contorno=rasgo.construirRasgo(tipo);
+	}
+
+	public rasgo getOjos() {
+		return ojos;
+	}
+
+	public rasgo getNariz() {
+		return nariz;
+	}
+
+	public rasgo getBoca() {
+		return boca;
+	}
+
+	public rasgo getContorno() {
+		return contorno;
+	}
+
+	public rasgo getCabello() {
+		return cabello;
+	}
+
+	public rasgo getCabeza() {
+		return cabeza;
+	}
+
+	public caminar_tipo caminar() {
+		return caminar;
+	}
+
+	public hablar_tipo hablar() {
+		return hablar;
+	}
+	public golpear_tipo golpear() {
+		return golpear;
+	}
+	
+	
+	public void set_hablar(hablar_tipo hablar) {
+		this.hablar= hablar;
+	}
+	public void set_caminar(caminar_tipo caminar) {
+		this.caminar= caminar;
+	}
+	public void set_golpear(golpear_tipo golpear) {
+		this.golpear= golpear;
+	}
+	
+	public static ArrayList<personaje> getPersonajes() {
+		return listaPersonajes;
+	}
+
+	@Override
+	public String toString() {
+		return nombre ;
+	}
+	public void añadirPersonaje() {
+		
+	}
+	
+}
